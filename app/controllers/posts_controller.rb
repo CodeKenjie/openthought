f2fcb3cfc1fd2b0241d6cec3e3b0c1e50fd6c4f3
KenjieTheGreate <franciskenjiem@gmail.com>
2026-08-07 09:28:31 +0800

    feat: create post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,20 @@
 class PostsController < ApplicationController
   def index
     @post = Post.new
+    @posts = Post.all
   end
 
   def show
   end
 
   def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      flash.now[:error] = @post.errors.full_messages.join(", ")
+      render :index, status: :unprocessable_entity
+    end
   end
 
   def edit
@@ -16,5 +24,11 @@ class PostsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def post_params
+    params.expect(post: [ :body ])
   end
 end
